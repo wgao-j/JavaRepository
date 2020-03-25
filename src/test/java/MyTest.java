45704dcf964f8bfe04efcb62816e871521f0319d
wgao <gaowei0310@163.com>
2020-03-25 10:50:01 +0800

    first submit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import static java.util.ResourceBundle.getBundle;
+
+public class MyTest extends TestCase {
+
+    String mylefor = "";
+
+    @Override
+    public int countTestCases() {
+        System.out.println("n");
+        return super.countTestCases();
+    }
+
+    public void testSout() {
+
+    }
+
+}
